9e926c0e61deefd825a41332b96421d1be8817f8
Jaggi <ArshdeepKaurJaggi>
2018-04-14 18:58:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/jaggi/event_page/activity_event.java
@@ -0,0 +1,14 @@
+package com.example.jaggi.event_page;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+public class activity_event extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event);
+
+    }
+}
